70e33394bf76c0d55b874ed6d1209532eeab4bb1
dhmec <dharmoradiya@gmail.com>
2020-10-27 14:06:48 -0600

    Add ValidateOneToOneHundredEven() in Program.cs

--- XUnitCumulativePractice/XUnitCumulativePractice/Program.cs
+++ XUnitCumulativePractice/XUnitCumulativePractice/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace XUnitCumulativePractice
 {
+    /*
+        ✅Create a method in XUnitCumulativePractice.Program.cs called ValidateOneToOneHundredEven() that will accept a string (“1” or “20” etc.)  as a parameter.
+        Validate that the string:
+        Is an integer (numeric, will safely parse to int).
+        Is between 1 and 100.
+        Is even.
+        If it is, return true. Otherwise return false.
+        Create test cases in Program_Tests.cs that will test each of the requirements of ValidateOneToOneHundredEven().
+        This is to say, we should be testing (Theories):
+
+     */
     public class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            ValidateOneToOneHundredEven("1");
+            ValidateOneToOneHundredEven("100");
+            ValidateOneToOneHundredEven("0");
+            ValidateOneToOneHundredEven("101");
+            ValidateOneToOneHundredEven("50");
+            ValidateOneToOneHundredEven("-2");
+                                   
         }
+        public static bool ValidateOneToOneHundredEven(string stringInput)
+        {
+            bool valid = false;
+
+            int intInput = int.Parse(stringInput);
+            if (intInput > 0 && intInput <= 100)
+            {
+                if (intInput % 2 == 0)
+                {
+                    Console.WriteLine("even");
+                    valid = true;
+                }
+                else
+                {
+                    Console.WriteLine("odd");
+                    valid = false;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter any number in between 1 to 100");
+                valid = false;
+            }
+            return valid;
+        }
+
     }
 }
